aae245d9c0076a04df4125cfefcaa225b4d8c245
Anshid O <anshidkoolimad@gmail.com>
2023-10-10 19:59:09 +0530

    first commit

---
+++ base/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+
+from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,TaskDelete,CustomLoginView
+urlpatterns = [
+    path('login/',CustomLoginView.as_view(),name='login'),
+    path('', TaskList.as_view(),name='tasks'),
+    path('logout/',LogoutView.as_view(next_page='login'),name='logout'),
+
+    path('task/<int:pk>/', TaskDetail.as_view(),name='task'),
+    path('task-create', TaskCreate.as_view(),name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(),name='task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(),name='task-delete'),
+]
